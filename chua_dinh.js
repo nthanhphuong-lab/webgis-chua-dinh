6e336a2df5adc1c049eebcc1d117e8a1ed5c759e
nthanhphuong-lab <nthanhphuong@agu.edu.vn>
2025-09-15 10:02:51 +0700

    Add files via upload

---
+++ chua_dinh.js
@@ -0,0 +1,44 @@
+var chua_dinh = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Chùa Hang (Phước Điền Tự)",
+        "type": "Chùa",
+        "description": "Ngôi chùa nổi tiếng dưới chân núi Sam, TP. Châu Đốc.",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/3/3f/Phuoc_Dien_Temple_An_Giang.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [105.08303, 10.68625]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Đình Châu Phú",
+        "type": "Đình",
+        "description": "Đình thờ Nguyễn Hữu Cảnh, di tích lịch sử tại Châu Đốc.",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/7/7c/Dinh_Chau_Phu_An_Giang.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [105.10440, 10.69845]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Chùa Tây An",
+        "type": "Chùa",
+        "description": "Ngôi chùa cổ có kiến trúc kết hợp Ấn - Việt - Hồi.",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/1/18/Tay_An_Pagoda_An_Giang.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [105.08197, 10.68608]
+      }
+    }
+  ]
+};
