4ba9267d711454c231948cee8aacbb87691b889a
nthanhphuong-lab <nthanhphuong@agu.edu.vn>
2026-03-25 09:24:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 // ===== MAP =====
-var map = L.map('map').setView([10.5, 105.3], 9);
+var map = L.map('map').setView([10.5, 105.3], 10);
 
-// Base layers
+// Base layer
 var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-var satellite = L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
-  subdomains: ['mt0','mt1','mt2','mt3']
-});
-
 osm.addTo(map);
-
-// Layer control
-L.control.layers({
-  "Bản đồ": osm,
-  "Vệ tinh": satellite
-}).addTo(map);
 
 // ===== GLOBAL =====
 let allData = [];
@@ -38,9 +28,22 @@ function renderMap(data) {
     let lat = f.geometry.coordinates[1];
     let lng = f.geometry.coordinates[0];
 
-    let marker = L.marker([lat, lng])
-      .bindPopup(`<b>${f.properties.name}</b><br>${f.properties.type}`);
+    // Popup chi tiết
+    let popupContent = `<b>${f.properties.name}</b><br>
+                        <b>Loại:</b> ${f.properties.type} - ${f.properties.subtype}<br>
+                        <b>Địa chỉ:</b> ${f.properties.address}<br>
+                        <b>Lịch sử:</b> ${f.properties.history}<br>
+                        <b>Văn hóa:</b> ${f.properties.culture}<br>
+                        <b>Kiến trúc:</b> ${f.properties.architecture}<br>`;
 
+    if (f.properties.attributes) {
+      popupContent += "<b>Chi tiết:</b><br>";
+      f.properties.attributes.forEach(attr => {
+        popupContent += `${attr.category}: ${attr.name} <br>`;
+      });
+    }
+
+    let marker = L.marker([lat, lng]).bindPopup(popupContent);
     marker.addTo(map);
     markers.push(marker);
   });
@@ -59,6 +62,8 @@ function renderList(data) {
       let lat = f.geometry.coordinates[1];
       let lng = f.geometry.coordinates[0];
       map.setView([lat, lng], 15);
+      markers.find(m => m.getLatLng().lat === lat && m.getLatLng().lng === lng)
+             .openPopup();
     };
 
     list.appendChild(li);
@@ -68,11 +73,11 @@ function renderList(data) {
 // ===== SEARCH =====
 document.getElementById("search").addEventListener("input", function() {
   let keyword = this.value.toLowerCase();
-
   let filtered = allData.filter(f =>
-    f.properties.name.toLowerCase().includes(keyword)
+    f.properties.name.toLowerCase().includes(keyword) ||
+    f.properties.type.toLowerCase().includes(keyword) ||
+    f.properties.subtype.toLowerCase().includes(keyword)
   );
-
   renderList(filtered);
   renderMap(filtered);
 });
@@ -83,41 +88,33 @@ function renderFilter(data) {
   filterDiv.innerHTML = "";
 
   let categories = new Set();
-
   data.forEach(f => {
     if (f.properties.attributes) {
-      f.properties.attributes.forEach(a => {
-        categories.add(a.category);
-      });
+      f.properties.attributes.forEach(a => categories.add(a.category));
     }
   });
 
   categories.forEach(cat => {
     let btn = document.createElement("button");
     btn.innerText = cat;
-
     btn.onclick = () => {
       let filtered = allData.filter(f =>
         f.properties.attributes &&
         f.properties.attributes.some(a => a.category === cat)
       );
-
       renderList(filtered);
       renderMap(filtered);
     };
-
     filterDiv.appendChild(btn);
   });
 
   // Reset button
   let reset = document.createElement("button");
   reset.innerText = "Tất cả";
-
   reset.onclick = () => {
     renderList(allData);
     renderMap(allData);
   };
-
   filterDiv.appendChild(reset);
 }
 
