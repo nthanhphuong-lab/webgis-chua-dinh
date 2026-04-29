b2d8dc609a1454725ef124889147969a8d377a50
nthanhphuong-lab <nthanhphuong@agu.edu.vn>
2026-04-29 15:53:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -28,19 +28,7 @@ Promise.all([
   applyFilter();
 })
 .catch(err => console.error("Lỗi load:", err));
-function translateInstruction(text) {
-  return text
-    .replace("Head", "Đi")
-    .replace("Turn right", "Rẽ phải")
-    .replace("Turn left", "Rẽ trái")
-    .replace("Continue", "Tiếp tục")
-    .replace("Merge right", "Nhập làn bên phải")
-    .replace("Merge left", "Nhập làn bên trái")
-    .replace("Keep right", "Giữ bên phải")
-    .replace("Keep left", "Giữ bên trái")
-    .replace("You have arrived at your destination", "Bạn đã đến nơi")
-    .replace("Destination", "Điểm đến");
-}
+
 function initMap() {
   map = L.map('map', { zoomControl: false })
     .setView([10.38, 105.43], 10);
@@ -356,20 +344,8 @@ function drawRoute(userLat, userLng, lat, lng) {
   const distance = (route.summary.totalDistance / 1000).toFixed(2);
   const time = Math.round(route.summary.totalTime / 60);
 
-  // 👇 hiển thị km + time
   document.getElementById("routeInfo").innerHTML =
     `📏 ${distance} km | ⏱ ${time} phút`;
-
-  // 👇 LẤY danh sách step
-  const steps = route.instructions;
-
-  let html = `<b>📍 Hướng dẫn:</b><br>`;
-
-  steps.forEach(s => {
-    html += "👉 " + translateInstruction(s.text) + "<br>";
-  });
-
-  document.getElementById("routeInfo").innerHTML += "<br>" + html;
 });
   
 }
